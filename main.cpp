076d65176dfda5ec7e805682c05554b2264adad8
Basmallaaa <132603976+Basmallaaa@users.noreply.github.com>
2025-07-13 19:05:30 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"a is greater than b";
+    }
+    else{
+        cout<<"b is greater than a";
+    }
+
+
+
+}
+
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
